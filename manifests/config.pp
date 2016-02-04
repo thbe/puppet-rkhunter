6d9c1da0312bada3261e94f5ac659b32415d26f8
thbe <project@bendler-net.de>
2016-02-04 19:42:33 +0100

    Switched to 0.6.0, see Changelog

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,37 +12,29 @@
 #
 class rkhunter::config {
 
-  file {
-    $rkhunter::params::config_rkhunter_conf:
-      ensure  => present,
-      mode    => '0640',
-      owner   => root,
-      group   => root,
-      path    => $rkhunter::params::config_rkhunter_conf,
-      content => template($rkhunter::params::config_rkhunter_conf_template);
-
-      $rkhunter::params::config_rkhunter_sys_conf:
-      ensure  => present,
-      mode    => '0640',
-      owner   => root,
-      group   => root,
-      path    => $rkhunter::params::config_rkhunter_sys_conf,
-      content => template($rkhunter::params::config_rkhunter_sys_conf_template);
+  file { $rkhunter::params::config_rkhunter_conf:
+    ensure  => file,
+    mode    => '0640',
+    path    => $rkhunter::params::config_rkhunter_conf,
+    content => template($rkhunter::params::config_rkhunter_conf_template);
   }
 
-  file {
-    $rkhunter::params::config_rkhunter_script_directory:
-      ensure  => directory,
-      mode    => '0755',
-      owner   => root,
-      group   => root;
+  file { $rkhunter::params::config_rkhunter_sys_conf:
+    ensure  => file,
+    mode    => '0640',
+    path    => $rkhunter::params::config_rkhunter_sys_conf,
+    content => template($rkhunter::params::config_rkhunter_sys_conf_template);
+  }
 
-    $rkhunter::params::config_rkhunter_script:
-      ensure  => present,
-      mode    => '0755',
-      owner   => root,
-      group   => root,
-      path    => $rkhunter::params::config_rkhunter_script,
-      content => template($rkhunter::params::config_rkhunter_script_template);
+  file { $rkhunter::params::config_rkhunter_script_directory:
+    ensure  => directory,
+    mode    => '0755';
+  }
+
+  file { $rkhunter::params::config_rkhunter_script:
+    ensure  => file,
+    mode    => '0755',
+    path    => $rkhunter::params::config_rkhunter_script,
+    content => template($rkhunter::params::config_rkhunter_script_template);
   }
 }
--- spec/classes/rkhunter_spec.rb
+++ spec/classes/rkhunter_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
-describe "rkhunter" do
+describe 'rkhunter', :type => :class do
+
+  context 'with defaults for all parameters' do
+    it { should contain_class('rkhunter') }
+  end
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
@@ -8,14 +12,57 @@ describe "rkhunter" do
         facts
       end
 
+      let(:params) {
+        {
+          :root_email => 'john.doe@example.com',
+          :remote_syslog => true,
+          :tftp => true,
+          :check_mk => true,
+          :oracle_xe => true,
+          :sap_daa => true,
+          :sap_icm => true,
+          :sap_db => true,
+          :sshd_root => 'without-password',
+          :web_cmd => 'curl',
+          :disable_tests => [ 'suspscan', 'hidden_procs', 'deleted_files', 'packet_cap_apps', 'apps' ]
+        }
+      }
+
       it { is_expected.to compile.with_all_deps }
 
       case facts[:osfamily]
       when 'Gentoo'
       when 'Debian'
       when 'RedHat'
-        if facts[:lsbmajdistrelease] = '5'
-        else
+        it { is_expected.to contain_class('rkhunter::package') }
+        it { is_expected.to contain_class('rkhunter::config') }
+        it { is_expected.to contain_class('rkhunter::service') }
+
+        it { is_expected.to contain_package('rkhunter').with_ensure('installed') }
+
+        it { is_expected.to contain_file('/etc/rkhunter.d').with_ensure('directory') }
+
+        it { is_expected.to contain_file('/etc/rkhunter.conf').with_ensure('file') }
+        it { is_expected.to contain_file('/etc/sysconfig/rkhunter').with_ensure('file') }
+        it { is_expected.to contain_file('/etc/rkhunter.d/checkWhiteList.sh').with_ensure('file') }
+
+        it 'should generate valid content for rkhunter.conf' do
+          content = catalogue.resource('file', '/etc/rkhunter.conf').send(:parameters)[:content]
+          expect(content).to match('SCRIPTDIR=/etc/rkhunter.d')
+          expect(content).to match('LOGFILE=/var/log/rkhunter/rkhunter.log')
+          expect(content).to match('ALLOW_SSH_ROOT_USER=without-password')
+          expect(content).to match('DISABLE_TESTS=suspscan hidden_procs deleted_files packet_cap_apps apps')
+          expect(content).to match('PKGMGR=RPM')
+          expect(content).to match('ALLOW_SYSLOG_REMOTE_LOGGING=1')
+          expect(content).to match('WEB_CMD=curl')
+          expect(content).to match('XINETD_ALLOWED_SVC=/etc/xinetd.d/tftp')
+          expect(content).to match('XINETD_ALLOWED_SVC=/etc/xinetd.d/check_mk')
+          expect(content).to match('ALLOWDEVFILE=/dev/shm/ora_XE_*')
+          expect(content).to match('PORT_PATH_WHITELIST=/usr/sap/DAA/SMDA97/exe/jstart:TCP:47107')
+          expect(content).to match('PORT_WHITELIST=TCP:25000')
+          expect(content).to match('ALLOWDEVFILE=/dev/shm/SDBTech-KSS-SHM-*')
+          expect(content).to match('EXISTWHITELIST=/var/log/pki-ca/system')
+          expect(content).to match('# FreeIPA Certificate Authority')
         end
       else
         it { is_expected.to contain_warning('The current operating system is not supported!') }
