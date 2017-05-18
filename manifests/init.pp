124f00ed28c1a1dc778180c7d3fc601115344154
Tudor Georgescu <T.Georgescu@kainos.com>
2017-05-18 15:35:11 +0100

    Fixing typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -74,7 +74,7 @@ class rkhunter (
   $sap_db               = $rkhunter::params::sap_db,
   $sshd_root            = $rkhunter::params::sshd_root,
   $web_cmd              = $rkhunter::params::web_cmd,
-  $disable_tests        = $rkhunter::params::disable_tests
+  $disable_tests        = $rkhunter::params::disable_tests,
   $cron_daily_run       = $rkhunter::params::cron_daily_run
 ) inherits rkhunter::params {
 
