816c23eb954cf987118abc34b604c808cb4c5094
thbe <thbe@users.noreply.github.com>
2019-05-15 11:04:37 +0200

    Rewrite spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,25 +2,34 @@ require 'beaker-puppet'
 require 'puppet'
 require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 require 'beaker/testmode_switcher'
 require 'beaker/testmode_switcher/dsl'
 
 # Install Puppet on all hosts
-install_puppet_agent_on(hosts, options)
+#install_puppet_agent_on(hosts, options)
+
+run_puppet_install_helper
+configure_type_defaults_on(hosts)
 
 RSpec.configure do |c|
-  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
+  #module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  
+  # Readable test descriptions
   c.formatter = :documentation
 
+  # Configure all nodes in nodeset
   c.before :suite do
     # Install module to all hosts
     hosts.each do |host|
       install_dev_puppet_module_on(host, :source => module_root, :module_name => 'rkhunter')
       # Install dependencies
-      on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
-
-      # Add more setup code as needed
+      install_module_dependencies_on(hosts)
+      # on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
     end
   end
+end
+
+def return_puppet_version
+  (on default, puppet('--version')).output.chomp
 end
